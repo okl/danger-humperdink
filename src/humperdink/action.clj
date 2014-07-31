451c2ca25e18abbb937d3b89c28e2b887162e6da
Matt Halverson <mhalverson@onekingslane.com>
2014-07-31 16:15:52 -0700

    Draft 1 of action interpreter

--- src/humperdink/action.clj
+++ src/humperdink/action.clj
@@ -8,39 +8,60 @@
   ;;(:require [humperdink.actions.log :as log-action])
   )
 
+(defn seqify [thing]
+  (if (or (seq? thing)
+          (vector? thing))
+    (seq thing)
+    (list thing)))
 
-;; (defprotocol EnvP
-;;   ""
-;;   (route [this] ""))
+;; # Type definitions
 
-;; (defprotocol ActionResultP
-;;   ""
-;;   (input [this] "")
-;;   (value [this] "")
-;;   (env [this] "")
-;;   (message [this] ""))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ??
 (defrecord Env [route custom-fields])
+(defrecord ActionInput [input env])
+(defrecord ActionOutput [output env])
 
 (defprotocol ActionP
   "docstring"
-  (invoke [this input ^Env env] "docstring"))
+  (invoke [this ^ActionInput action-input] "returns a seq of ActionOutputs"))
 
-(deftype Action [fxn]
+(defrecord Action [fxn]
+  ;; The fxn should take an input and env,
+  ;; and return an output and possibly different env.
   ActionP
-  (invoke [this input env]
-    (fxn input env)))
+  (invoke [this action-input]
+    (let [input (:input action-input)
+          env (:env action-input)
+          [output env'] (fxn input env)]
+      (list (->ActionOutput output env')))))
 
-(defrecord ActionResult [input output ^Env env message])
+(defrecord MultiAction [fxn]
+  ;; The fxn should take an input and env,
+  ;; and return a seq of output/possibly-different-env pairs.
+  ;; A la denormalizing an array.
+  ActionP
+  (invoke [this action-input]
+    (let [input (:input action-input)
+          env (:env action-input)
+          output-env-pairs (fxn input env)]
+      (map #(->ActionOutput (first %) (second %)) output-env-pairs))))
 
+;; # Sample action definitions
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;(defaction log-to-disk log-action/make-log-to-disk)
 
-(def a1 (->Action str {}))
-;;(defaction a1 [val env] val)
-(def a-identity (->Action (fn [val env] val)
-                  {}))
-(value a-identity 42)
+(def a1 (->Action
+         (fn [a b]
+           [(str a) {}])))
+(invoke a1 (->ActionInput 42 {}))
+
+(def a-identity
+  (->Action (fn [val env]
+              [val (assoc env :t (System/currentTimeMillis))])))
+(invoke a-identity (->ActionInput 42 {}))
+
+(def multi-a
+  (->MultiAction (fn [val env]
+                   [[val env]
+                    [(inc val) (assoc env :i :gotcha)]
+                    [(* 2 val) (assoc env :i :doubled)]])))
+(invoke multi-a (->ActionInput 3 {}))
--- src/humperdink/interp.clj
+++ src/humperdink/interp.clj
@@ -1,11 +1,19 @@
 (ns humperdink.interp
-  "Defines an action interpreter"
+  "Defines an action interpreter, for building up big complex Actions"
   {:author "Matt Halverson"
    :date "2014/07/10"}
   (:require [roxxi.utils.print :refer [print-expr]]
             [roxxi.utils.common :refer [def-]])
+  (:require [humperdink.action :refer [invoke
+                                       ActionP
+                                       ->Action
+                                       ->MultiAction
+                                       ->ActionInput
+                                       ->ActionOutput
+                                       seqify]])
   (:require [clojure.tools.logging :as log])
-  (:require [diesel.core :refer [definterpreter]]))
+  (:require [diesel.core :refer [definterpreter]])
+  (:require [denormal.core :refer [denormalize-map]]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (def- act-maker-reg (atom {}))
@@ -28,12 +36,6 @@ act-maker :: [initialization args] -> (input -> env -> output)
 (defn action-maker-registry [] @act-maker-reg)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn- seqify [thing]
-  (if (or (seq? thing)
-          (vector? thing))
-    (seq thing)
-    (list thing)))
-
 (def begins-with-symbol? (comp symbol? first))
 
 (definterpreter interp [reg]
@@ -45,27 +47,26 @@ act-maker :: [initialization args] -> (input -> env -> output)
   [':ref => :ref]
   [begins-with-symbol? => :simple-action])
 
-(defn- action-applier
-  "Takes a collection of actions s.t. each action takes a value x env => value*
-and returns a function that invokes each supplied function on value*
+(defn- in->out [action-input]
+  (->ActionOutput (:input action-input) (:env action-input)))
+(defn- out->in [action-output]
+  (->ActionInput (:output action-output) (:env action-output)))
 
-value* is short hand for a sequence of 0 or more values
-"
-  [actions]
-  (fn [value* env]
-    (loop [results (seqify value*)
-           actions actions]
-      (if (empty? actions)
-        (seqify results)
-        (recur (mapcat #((first actions) % env) results)
-               (rest actions))))))
-
-(defmethod interp :sequence [[_ & exprs] reg]
+(defmethod interp :sequence [[token & exprs] reg]
+  ;; Daisy-chains a bunch of Actions together, feeding the ActionOutput(s)
+  ;; of each Action as ActionInputs into the next."
   (println "called sequence")
-  (let [interped (map #(interp % reg) exprs)
-        action-fn (action-applier interped)]
-    (fn [val env]
-      (action-fn val env))))
+  (let [sub-actions (map #(interp % reg) exprs)]
+    (reify ActionP
+      (invoke [_ action-input]
+        (loop [prev-action-outputs (list (in->out action-input))
+               actions sub-actions]
+          (if (empty? actions)
+            prev-action-outputs
+            (let [curr-action (first actions)
+                  curr-action-inputs (map out->in prev-action-outputs)]
+              (recur (mapcat #(invoke curr-action %) curr-action-inputs)
+                     (rest actions)))))))))
 
 (defmethod interp :parallel [[token & exprs] reg]
   ;;require at least one branch.
@@ -73,9 +74,10 @@ value* is short hand for a sequence of 0 or more values
   (println "called parallel")
   (when (empty? exprs)
     (log/errorf "Called %s with 0 args. Behavior will be undefined." token))
-  (let [interped (map #(interp % reg) exprs)]
-    (fn [val env]
-      (seqify (last (pmap #(% val env) interped))))))
+  (let [sub-actions (map #(interp % reg) exprs)]
+    (reify ActionP
+      (invoke [_ action-input]
+        (last (pmap #(invoke % action-input) sub-actions))))))
 
 (defmethod interp :ref [[_ name] reg]
   (println "called ref")
@@ -85,10 +87,10 @@ value* is short hand for a sequence of 0 or more values
 ;; XXX here is where to honor "log every step" option?
 (defmethod interp :simple-action [[name & init-args] reg]
   (println "called simple-action")
-  (let [fxn-maker (get reg name)
+  (let [action-maker (get reg name)
         looked-up-refs (map #(interp % reg) init-args)
-        action (apply fxn-maker looked-up-refs)]
-    (comp seqify action)))
+        action (apply action-maker looked-up-refs)]
+    action))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -116,68 +118,115 @@ value* is short hand for a sequence of 0 or more values
 (do
   (defactionmaker 'jawsome-xform-99
     (fn [init-arg]
-      (fn [val env] (str val " " init-arg " " env))))
+      (->Action (fn [val env] [(str val " " init-arg) env]))))
   (defvar 'init-arg-99 "nevergonnagiveyouup")
-  (def p1 (interp '(jawsome-xform-99 ["going back down to Georgia" (+ 2 2)])
+  (def p1 (interp '(jawsome-xform-99 (ref init-arg-99))
                   (action-maker-registry)))
-  (p1 42 {}))
+  p1
+  (invoke p1 (->ActionInput 42 {})))
 
 ;; Parallel action
 
 (do
   (defactionmaker 'print-1
     (fn []
-      (fn [val env]
-        (println (str "gooo print-1, value is " val "!"))
-        val)))
+      (->Action (fn [val env]
+                  (println (str "gooo print-1, value is " val "!"))
+                  ["print1" env]))))
   (defactionmaker 'print-2
     (fn []
-      (fn [val env]
-        (println (str "huzzah print-2, value is " val "!"))
-        (* val 2))))
+      (->Action (fn [val env]
+                  (println (str "huzzah print-2, value is " val "!"))
+                  ["print2" env]))))
   (def p2 (interp '(|| (print-1)
                        (print-2))
                   (action-maker-registry)))
-  (p2 42 {}))
+  (invoke p2 (->ActionInput 42 {})))
 
 ;; Composite action
 
 (do
   (defactionmaker 'inc
     (fn []
-      (fn [val env]
-;;        [(inc val) env])))
-        (inc val))))
+      (->Action (fn [val env]
+        [(inc val) env]))))
+;;        (inc val))))
   (defactionmaker 'log
     (fn []
-      (fn [val env]
-        (println (str "val is " val))
-;;        [val env])))
-        val)))
+      (->Action (fn [val env]
+                  (println (str "val is " val ", env is " env))
+                  [val env]))))
+;;        val)))
   (defactionmaker 'double
     (fn []
-      (fn [val env]
-;;        [(* val 2) env])))
-        (* val 2))))
+      (->Action (fn [val env]
+                  [(* val 2) env]))))
+;;        (* val 2))))
   (defactionmaker 'mult
     (fn [factor]
-      (fn [val env]
-;;        [(* val factor) env])))
-        (* val factor))))
+      (->Action (fn [val env]
+                  [(* val factor) env]))))
+;;        (* val factor))))
+  (defactionmaker 'fork
+    (fn []
+      (->MultiAction (fn [val env]
+                       [[val (assoc env :fork 1)]
+                        [val (assoc env :fork 2)]]))))
   (def p3 (interp '(-> (log)
                        (double)
                        (log)
                        (mult 5)
                        (log)
                        (inc)
+                       (log)
+                       (fork)
                        (log))
                   (action-maker-registry)))
-  (p3 16 {})
+  (invoke p3 (->ActionInput 16 {})))
+
+(do
   (defactionmaker 'p3
     (constantly p3))
-  (def p4 (interp '(-> (log)
-                       (p3)
+  (def p4 (interp '(-> (p3)
                        (mult 10)
                        (log))
                   (action-maker-registry)))
-  (p4 16 {}))
+  (invoke p4 (->ActionInput 16 {}))
+  )
+
+;; parallel of seqs
+
+(do
+  (defactionmaker 'read-json
+    (fn []
+      (->Action (fn [val env]
+                  [(read-string val) env]))))
+  (defactionmaker 'denorm
+    (fn []
+      (->MultiAction (fn [val env]
+                       (let [vals (denormalize-map val)
+                             val-env-pairs (map #(vector % env) vals)]
+                         val-env-pairs)))))
+  (def p-of-s (interp '(|| (|| (print-1)
+                               (print-1))
+                           (log)
+                           (-> (read-json)
+                               (denorm)
+                               (log))
+                           (-> (print-2)
+                               (jawsome-xform-99 (ref init-arg-99))))
+                  (action-maker-registry)))
+  (invoke p-of-s (->ActionInput "{\"a\" [1 2 3], \"b\" 324}" {}))
+  )
+
+
+;; seq of parallels
+(do
+  (def s-of-p (interp '(-> (|| (log)
+                               (-> (inc)
+                                   (log)))
+                           (|| (print-1)
+                               (print-2)))
+                      (action-maker-registry)))
+  (invoke s-of-p (->ActionInput 42 {}))
+  )
